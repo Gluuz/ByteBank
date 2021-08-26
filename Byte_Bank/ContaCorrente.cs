567d764cab2abd0e7e8ae9e27d7086756babbd9f
Gluuz <brunogluszczukk1@gmail.com>
2021-08-26 17:04:30 -0300

    First commit

---
+++ Byte_Bank/ContaCorrente.cs
@@ -0,0 +1,20 @@
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public bool Sacar(double valor)
+    {
+        if(this.saldo <= valor){
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        //test
+    }
+}
---
+++ Byte_Bank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Byte_Bank
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente ContaDoJoao = new ContaCorrente();
+
+            //ContaDoJoao.titular= "João";
+            //ContaDoJoao.agencia= 100;
+            ContaDoJoao.numero= 666;
+            ContaDoJoao.saldo= 100;
+            
+            Console.WriteLine("Nome: " + ContaDoJoao.titular);
+            Console.WriteLine("Agencia:" + ContaDoJoao.agencia);
+            Console.WriteLine("Número:" + ContaDoJoao.numero);
+            Console.WriteLine("Saldo:" + ContaDoJoao.saldo);
+
+            ContaDoJoao.Sacar(20);
+            Console.WriteLine(ContaDoJoao.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
