5a018c3e57b419920c9e6223974dd21eee5df550
Gluuz <brunogluszczukk1@gmail.com>
2021-08-27 15:58:27 -0300

    Classe Funcionario

---
+++ Byte_Bank/Funcionarios/Funcionario.cs
@@ -0,0 +1,12 @@
+namespace Byte_Bank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        public double Salario { get; set; }
+        
+
+    }
+}
