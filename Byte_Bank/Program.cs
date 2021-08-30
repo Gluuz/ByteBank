d586d20173c479c837889f52b7cf239d4603c573
Gluuz <brunogluszczukk1@gmail.com>
2021-08-30 15:53:16 -0300

    Teste de Sistema de senha

--- Byte_Bank/Program.cs
+++ Byte_Bank/Program.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 using Byte_Bank.Funcionarios;
+using Byte_Bank.SistemaInterno;
 namespace Byte_Bank
 {
   class Program
     {
         static void Main(string[] args)
         {
-            Funcionario Bruno = new Auxiliar(2000, "468548545");
-            Bruno.Nome = "Bruno";
-            Bruno.AumentaSalario();
-            Console.WriteLine(Bruno.Salario);
-            
-            
-            Funcionario Roberta = new GerenteDeConta(4000, "471568765");
-            Roberta.AumentaSalario();
-            Console.WriteLine(Roberta.Salario);
-
-            Funcionario Mauricio = new Designer(3000, "4568135186");
-            Mauricio.AumentaSalario();
-            Console.WriteLine(Mauricio.Salario);
-
-            Funcionario Paulo = new Diretor(5000, "65445132874");
-            Paulo.AumentaSalario();
-            Console.WriteLine(Paulo.Salario);
-
+            UsarSistema();
             Console.ReadLine();
+            
+            
+        }
+
+        private static void UsarSistema()
+        {
+            Sistema sistemaInterno = new Sistema();
+            Diretor roberta = new Diretor(5000, "4");
+            roberta.Nome = "Roberta";
+            roberta.Senha = "123";
+
+            sistemaInterno.Logar(roberta, "123");
+            sistemaInterno.Logar(roberta, "312");
+
+            
         }
     }
 }
