b5a58eead6b49ebcfdb6fb4e50a31be43497d599
Gluuz <brunogluszczukk1@gmail.com>
2021-09-01 11:46:59 -0300

    Testando Exceção

--- Byte_Bank/Program.cs
+++ Byte_Bank/Program.cs
@@ -7,27 +7,18 @@ namespace Byte_Bank
     {
         static void Main(string[] args)
         {
-            UsarSistema();
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(0, 0);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Ocorreu uma excessão do tipo ArgumentException");         
+                Console.WriteLine(e.Message);  
+            }
+            
             Console.ReadLine();
             
-            
-        }
-
-        private static void UsarSistema()
-        {
-            Sistema sistemaInterno = new Sistema();
-            Diretor roberta = new Diretor(5000, "4");
-            roberta.Nome = "Roberta";
-            roberta.Senha = "123";
-            ParceiroComercial parceiro = new ParceiroComercial();
-            parceiro.Senha= "321";
-
-            sistemaInterno.Logar(roberta, "123");
-            sistemaInterno.Logar(roberta, "321");
-            sistemaInterno.Logar(parceiro, "123");
-            sistemaInterno.Logar(parceiro, "321");
-            
-
             
         }
     }
